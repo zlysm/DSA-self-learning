d65ad787e778c5c66d88faca8adb56876fb7c657
zlysm <zym0622@outlook.com>
2022-08-29 23:03:18 +0800

    merge sort

--- Vec.h
+++ Vec.h
@@ -217,4 +217,28 @@ bool Vector<T>::bubble(Rank lo, Rank hi) {
     return sorted;
 }
 
+template<typename T>
+void Vector<T>::mergeSort(Rank lo, Rank hi) {
+    if (hi - lo < 2) return;
+    int mi = (lo + hi) >> 1;
+    mergeSort(lo, mi);  //分别排序
+    mergeSort(mi, hi);
+    merge(lo, mi, hi);  //归并
+}
+
+template<typename T>
+void Vector<T>::merge(Rank lo, Rank mi, Rank hi) {
+    T *A = _elem + lo;  //合幵后癿向量A[0, hi - lo) = _elem[lo, hi)
+    int lb = mi - lo;
+    T *B = new T[lb];  //前子向量B[0, lb) = _elem[lo, mi)
+    for (Rank i = 0; i < lb; B[i] = A[i++]); //复刢前子向量
+    int lc = hi - mi;
+    T *C = _elem + mi;  //后子向量C[0, lc) = _elem[mi, hi)
+    for (Rank i = 0, j = 0, k = 0; (j < lb) || (k < lc);) {  //B[j]和C[k]中癿小者续至A末尾
+        if ((j < lb) && (!(k < lc) || (B[j] <= C[k]))) A[i++] = B[j++];
+        if ((k < lc) && (!(j < lb) || (C[k] <= B[j]))) A[i++] = C[k++];
+    }
+    delete[] B;
+}  //弻幵后得刡完整癿有序向量[lo, hi)
+
 #endif //DSA_VEC_H
