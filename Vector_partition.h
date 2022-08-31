931e676a233320e6ba36fba14b5f8764a5ceec29
Yiming Zhang <88626701+zlysm@users.noreply.github.com>
2022-08-31 18:37:04 +0800

    bug fixed

--- Vector_partition.h
+++ Vector_partition.h
@@ -9,7 +9,7 @@
 template<typename T>
 //轴点构造算法：通过调整元素位置构造区间[lo, hi)的轴点，并返回其秩
 Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
-    swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
+    std::swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
     T pivot = _elem[lo]; //以首元素为候选轴点——经以上交换，等效于随机选取
     int mi = lo;
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -20,14 +20,14 @@ Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     for (Rank k = lo + 1; k < hi; k++) //自左向右扫描
         if (_elem[k] < pivot) //若当前元素_elem[k]小于pivot，则
-            swap(_elem[++mi], _elem[k]); //将_elem[k]交换至原mi之后，使L子序列向右扩展
+            std::swap(_elem[++mi], _elem[k]); //将_elem[k]交换至原mi之后，使L子序列向右扩展
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //   --------- L < [lo] ---------- ] ------------- [lo] <= G ----------]
     // X x . . . . . . . . . . . . . . x . . . . . . . . . . . . . . . . . x X
     // |                               |                                     |
     // lo (pivot candidate)            mi                                    hi
     //++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-    swap(_elem[lo], _elem[mi]); //候选轴点归位
+    std::swap(_elem[lo], _elem[mi]); //候选轴点归位
     return mi; //返回轴点的秩
 }
 
@@ -35,7 +35,7 @@ Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
 //template<typename T>
 ////轴点构造算法：通过调整元素位置构造区间[lo, hi)的轴点，并返回其秩
 //Rank Vector<T>::partition(Rank lo, Rank hi) { //DUP版：可优化处理多个关键码雷同的退化情况
-//    swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
+//    std::swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
 //    hi--;
 //    T pivot = _elem[lo]; //以首元素为候选轴点——经以上交换，等效于随机选取
 //    while (lo < hi) { //从向量的两端交替地向中间扫描
@@ -64,7 +64,7 @@ Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
 //template<typename T>
 ////轴点构造算法：通过调整元素位置构造区间[lo, hi)的轴点，并返回其秩
 //Rank Vector<T>::partition(Rank lo, Rank hi) { //DUP1版：与DUP版等价，类似于与LUG版等价的LUG1版
-//    swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
+//    std::swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
 //    hi--;
 //    T pivot = _elem[lo]; //以首元素为候选轴点——经以上交换，等效于随机选取
 //    while (lo < hi) { //从向量的两端交替地向中间扫描
@@ -83,7 +83,7 @@ Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
 //template<typename T>
 ////轴点构造算法：通过调整元素位置构造区间[lo, hi)的轴点，并返回其秩
 //Rank Vector<T>::partition(Rank lo, Rank hi) { //LUG版本：基本形式
-//    swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
+//    std::swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
 //    hi--;
 //    T pivot = _elem[lo]; //以首元素为候选轴点——经以上交换，等效于随机选取
 //    while (lo < hi) { //从向量的两端交替地向中间扫描
@@ -102,7 +102,7 @@ Rank Vector<T>::partition(Rank lo, Rank hi) { //LGU版
 //template<typename T>
 ////轴点构造算法：通过调整元素位置构造区间[lo, hi)的轴点，并返回其秩
 //Rank Vector<T>::partition(Rank lo, Rank hi) { //LUG1版：与LUG版等价，类似于与DUP版等价的DUP1版
-//    swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
+//    std::swap(_elem[lo], _elem[lo + rand() % (hi - lo)]); //任选一个元素与首元素交换
 //    hi--;
 //    T pivot = _elem[lo]; //以首元素为候选轴点——经以上交换，等效于随机选取
 //    while (lo < hi) { //从向量的两端交替地向中间扫描
