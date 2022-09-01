be66459dc7165958580d1d45410305b0a62ab417
Yiming Zhang <zym0622@outlook.com>
2022-09-01 17:39:12 +0800

    create ListNode template

--- ListNode.h
+++ ListNode.h
@@ -5,23 +5,28 @@
 #ifndef DSA_LISTNODE_H
 #define DSA_LISTNODE_H
 
-typedef int Rank;
-#define ListNodePosi(T) ListNode<T>
+using Rank = int; //秩
+
+template<typename T>
+struct ListNode;
+
+template<typename T> using ListNodePosi = ListNode<T> *;   //列表节点位置
 
 template<typename T>
 struct ListNode {
     //成员
     T data;
-    ListNodePosi(T) pred;
-    ListNodePosi(T) succ;  //数值、前驱、后继
+    ListNodePosi<T> pred;
+    ListNodePosi<T> succ;  //数值、前驱、后继
     //constructor
     ListNode() {};  //针对header和trailer癿极造
 
-    ListNode(T e, ListNodePosi(T) p = NULL, ListNodePosi(T) s = NULL) : data(e), pred(p), succ(s) {}
-    //操作接口
-    ListNodePosi(T) insertAsPred(T const &e);  //紧靠弼前节点之前揑入新节点
+    ListNode(T e, ListNodePosi<T> p = NULL, ListNodePosi<T> s = NULL) : data(e), pred(p), succ(s) {}
 
-    ListNodePosi(T) insertAsSucc(T const &e);
+    //操作接口
+    ListNodePosi<T> insertAsPred(T const &e);  //紧靠弼前节点之前揑入新节点
+
+    ListNodePosi<T> insertAsSucc(T const &e);
 };
 
 #endif //DSA_LISTNODE_H
