d6c11e6a2467274863a74ed78e2a4c513fb82400
Yiming Zhang <zym0622@outlook.com>
2022-09-01 21:07:12 +0800

    overload [] and define find function

--- List_realization.h
+++ List_realization.h
@@ -16,4 +16,18 @@ void List<T>::init() {
     _size = 0;
 }
 
+template<typename T>
+T &List<T>::operator[](Rank r) const {
+    ListNodePosi<T> p = first();
+    while (0 < r--) p = p->succ;  //顺数第r个节点即是
+    return p->data;  //目标节点，迒回其中所存元素
+}
+
+template<typename T>
+ListNodePosi<T> List<T>::find(const T &e, int n, ListNodePosi<T> p) const {
+    while (0 < n--)  //（0 <= n <= rank(p) < _size）对亍p癿最近癿n个前驱，从右向左
+        if (e == (p = p->pred)->data) return p;
+    return NULL;  //p越出左边界意味着匙间内丌含e，查找失败
+}
+
 #endif //DSA_LIST_REALIZATION_H
