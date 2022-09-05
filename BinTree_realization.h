d186f5d00a3c103c9e10e8b3ec357dab01e1c1bd
Yiming Zhang <zym0622@outlook.com>
2022-09-05 21:46:02 +0800

    define remove

--- BinTree_realization.h
+++ BinTree_realization.h
@@ -63,4 +63,22 @@ BinNodePosi<T> BinTree<T>::attach(BinNodePosi<T> x, BinTree<T> *&S) { //x->rc ==
     return x; //释放原树，返回接入位置
 }
 
+template<typename T>
+int BinTree<T>::remove(BinNodePosi<T> x) {  //删除二叉树中位置x处的节点及其后代，返回被删除节点的数值
+    FromParentTo(*x) = NULL;  //切断来自父节点的指针
+    updateHeightAbove(x->parent);
+    int n = removeAt(x);
+    _size -= n;
+    return n;  //删除子树x，更新规模，返回删除节点总数
+}
+
+template<typename T>
+static int removeAt(BinNodePosi<T> x) {
+    if (!x) return 0;  //递归基：空树
+    int n = 1 + removeAt(x->lc) + removeAt(x->rc);  //递归释放左、右子树
+    release(x->data);
+    release(x);
+    return n;
+}
+
 #endif //DSA_BINTREE_REALIZATION_H
