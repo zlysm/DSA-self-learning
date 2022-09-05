ef10d4e3fcdfef508b0eb0323f288868b66edfd9
Yiming Zhang <zym0622@outlook.com>
2022-09-05 21:10:20 +0800

    define insert

--- BinTree_realization.h
+++ BinTree_realization.h
@@ -17,4 +17,26 @@ void BinTree<T>::updateHeightAbove(BinNodePosi<T> x) {  //更新高度
     }
 }
 
+template<typename T>
+BinNodePosi<T> BinTree<T>::insert(const T &e) {
+    _size = 1;
+    return _root = new BinNode<T>(e);  //将e当作根节点插入空的二叉树
+}
+
+template<typename T>
+BinNodePosi<T> BinTree<T>::insert(const T &e, BinNodePosi<T> x) {  //e插入为x的左孩子
+    _size++;
+    x.insertAsLC(e);
+    updateHeightAbove(x);
+    return x->lc;
+}
+
+template<typename T>
+BinNodePosi<T> BinTree<T>::insert(BinNodePosi<T> x, const T &e) {  //e插入为x的右孩子
+    _size++;
+    x.insertAsRC(e);
+    updateHeightAbove(x);
+    return x->rc;
+}
+
 #endif //DSA_BINTREE_REALIZATION_H
