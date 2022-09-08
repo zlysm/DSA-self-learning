e4df4f750ed0d54629cba179108d5639a4a5de4e
Yiming Zhang <zym0622@outlook.com>
2022-09-08 22:04:59 +0800

    define succ

--- BinNode_realization.h
+++ BinNode_realization.h
@@ -23,4 +23,17 @@ int BinNode<T>::size() { //统计当前节点后代总数，即以其为根的�
     return s;
 }
 
+template<typename T>
+BinNodePosi<T> BinNode<T>::succ() { //定位节点v的直接后继
+    BinNodePosi<T> s = this; //记录后继的临时变量
+    if (rc) { //若有右孩子，则直接后继必在右子树中，具体地就是
+        s = rc; //右子树中
+        while (HasLChild(*s)) s = s->lc; //最靠左（最小）的节点
+    } else { //否则，直接后继应是“将当前节点包含于其左子树中的最低祖先”，具体地就是
+        while (IsRChild(*s)) s = s->parent; //逆向地沿右向分支，不断朝左上方移动
+        s = s->parent; //最后再朝右上方移动一步，即抵达直接后继（如果存在）
+    }
+    return s;
+}
+
 #endif //DSA_BINNODE_REALIZATION_H
