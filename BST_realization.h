833b89679c77465d4c7dba345ed7cd114ab8d232
Yiming Zhang <zym0622@outlook.com>
2022-10-24 23:47:11 +0800

    use search to complete insert

--- BST_realization.h
+++ BST_realization.h
@@ -36,15 +36,25 @@ BinNodePosi<T> BST<T>::connect34(
 
 template<typename T>
 BinNodePosi<T> &BST<T>::search(const T &e) { //在BST中查找关键码e
-    if (!_root || e == _root->data) {
+    if (!BinTree<T>::_root || e == BinTree<T>::_root->data) {
         _hot = NULL;
-        return _root;
+        return BinTree<T>::_root;
     } //空树，或恰在树根命中
-    for (_hot = _root;;) { //否则，自顶而下
+    for (_hot = BinTree<T>::_root;;) { //否则，自顶而下
         BinNodePosi<T> &v = (e < _hot->data) ? _hot->lc : _hot->rc; //确定方向，深入一层
         if (!v || e == v->data) return v;
         _hot = v; //一旦命中或抵达叶子，随即返回
     } //返回目标节点位置的引用，以便后续插入、删除操作
 } //无论命中或失败，_hot均指向v之父亲（v是根时，hot为NULL）
+
+template<typename T>
+BinNodePosi<T> BST<T>::insert(const T &e) {
+    BinNodePosi<T> &x = search(e);
+    if (x) return x;
+    x = new BinNode<T>(e, _hot);
+    ++BinTree<T>::_size;
+    BinTree<T>::updateHeightAbove(x);
+    return x;
+}
 
 #endif //DSA_BST_REALIZATION_H
