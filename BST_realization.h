9e5edf9188fded4af277dfccf0031421a057a42e
Yiming Zhang <zym0622@outlook.com>
2022-10-25 00:13:20 +0800

    remove func (confused)

--- BST_realization.h
+++ BST_realization.h
@@ -57,6 +57,36 @@ BinNodePosi<T> BST<T>::insert(const T &e) {
     return x;
 }
 
+template<typename T>
+static BinNodePosi<T> removeAt(BinNodePosi<T> &x, BinNodePosi<T> &hot) {
+    BinNodePosi<T> w = x;
+    BinNodePosi<T> succ = NULL;
+    if (!HasLChild (*x))   //若*x的左子树为空，则可直接将*x替换为其右子树，左子树同理
+        succ = x = x->rc;
+    else if (!HasRChild (*x))
+        succ = x = x->lc;
+    else { //若左右子树均存在，则选择x的直接后继作为实际被摘除节点，为此需要
+        w = w->succ(); //（在右子树中）找到*x的直接后继*w
+        std::swap(x->data, w->data); //交换*x和*w的数据元素
+        BinNodePosi<T> u = w->parent;
+        ((u == x) ? u->rc : u->lc) = succ = w->rc; //隔离节点*w   ?????
+    }
+    hot = w->parent; //记录实际被删除节点的父亲
+    if (succ) succ->parent = hot; //并将被删除节点的接替者与hot相联
+    release(w->data);
+    release(w);
+    return succ; //释放被摘除节点，返回接替者
+}
+
+template<typename T>
+bool BST<T>::remove(const T &e) {
+    BinNodePosi<T> &x = search(e);
+    if (!x) return false;
+    removeAt(x, _hot);
+    BinTree<T>::_size--;
+    BinTree<T>::updateHeightAbove(_hot);
+    return true;
+}
 
 
 #endif //DSA_BST_REALIZATION_H
