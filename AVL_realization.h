fd5e97a413069a3914e08cb0afb50ac98456a6f5
Yiming Zhang <zym0622@outlook.com>
2022-11-01 00:09:45 +0800

    bug fixed: rotateAt

--- AVL_realization.h
+++ AVL_realization.h
@@ -14,7 +14,7 @@ BinNodePosi<T> AVL<T>::insert(const T &e) {
     for (BinNodePosi<T> g = AVL<T>::_hot; g; g = g->parent)  //从x之父出发向上，逐层检查各代祖先g
         if (!AvlBalanced(*g)) {  //can't use "FromParentTo" marco due to template class
             IsRoot(*g) ? this->_root : (IsLChild(*g) ? (*g).parent->lc : (*g).parent->rc)
-                                               = rotateAt(tallerChild(g));
+                                               = rotateAt(tallerChild(tallerChild(g)));
             break;  //一旦发现g失衡，则（采用“3 + 4”算法）使之复衡，并将子树重新接入原树
         } else  //未失衡只需更新其高度（注意：即便g未失衡，高度亦可能增加）
             updateHeight(g);
